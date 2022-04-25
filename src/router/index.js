729fe9e23a1c6e44c958a94f44c69ed1796ffc0e
shivam3012 <shivamverma3012@gmail.com>
2022-04-25 20:44:11 +0530

    unused code removed

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const SignatureController = require("../controller/signature");
-const service_auth = require("./auth.tracker");
 
 /** GET /health-check - Check service health */
 router.get('/health-check', (req, res) =>

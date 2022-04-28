4ccdd8f9f9560b04123674a7ae99442f5d05fe65
shivam3012 <shivamverma3012@gmail.com>
2022-04-28 22:02:54 +0530

    update private key

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     PORT: 5000,
-    PRIVATE_KEY: "d8b1934a4fa6774c73e4d43fbc51de82664879affa75aebe48f15e1e7e6c8604",
+    PRIVATE_KEY: "33f6adcb7acf42f1616b84c4a3efef462ab9308cc2c449a5797b1e429e02e0f4",
     ETH_HTTP: "https://rpc.ankr.com/eth",
     //Ethereum
-    CHAIN_ID: [1, 4],
+    CHAIN_ID: [1],
     WHITELIST: [
         "0x45024a24bFDFB14864b750B89e33E30384DE6a76",
         "0x319D2e25F82c821F8cE4171bbE576d42d74d1277",
